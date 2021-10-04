90703ef96669f0dbb8194bbae5d1cbf21355c4ac
Proshanta Debnath <73601711+ProshantaDebnath@users.noreply.github.com>
2021-10-05 03:15:17 +0530

    add Navbar, dummy data & structure project

---
+++ frontend/src/Components/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a className="navbar-brand" href="#">Domaincer</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item ">
+                                <a className="nav-link" href="#">LogIn</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">LogOut</a>
+                            </li>
+
+                        </ul>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ frontend/src/jobs.js
@@ -0,0 +1,22 @@
+const jobs = [{
+  "position": "Jr. Software Engineer",
+  "experience": [
+    "1 year",
+    "2 year",
+    "3 year"
+  ],
+  "image": "https://i.ytimg.com/vi/_3v3wVihHj8/maxresdefault.jpg",
+  "skill": "C++, java, python, javascript"
+}, {
+  "position": "Sr. Software Engineer",
+  "experience": [
+    "4 year",
+    "5 year",
+    "6 year"
+  ],
+  "image": "https://sekal.com/wp-content/uploads/2018/04/shutterstock_645324130-1080x675.jpg",
+  "skill": "C++, java, python, javascript"
+}
+]
+
+export default jobs;
