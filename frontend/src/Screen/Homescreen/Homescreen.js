2c461a4d471d868cf73be821fdacdd807336b13e
Proshanta Debnath <73601711+ProshantaDebnath@users.noreply.github.com>
2021-10-07 00:55:13 +0530

    modify some styling

--- frontend/src/Screen/Homescreen/Homescreen.js
+++ frontend/src/Screen/Homescreen/Homescreen.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getAllJobs } from '../../actions/jobActions'
 import Jobs from '../../Components/Jobs/Jobs'
+import './Homescreen.css'
 //import jobs from '../../jobs'
 
 export default function Homescreen() {
@@ -21,9 +22,9 @@ export default function Homescreen() {
             <div className="row">
 
                 {loading ? (
-                    <h1>Loading...</h1>
+                    <h1 className='loading'>Loading...</h1>
                 ) : error ? (
-                    <h1>Something Went Wrong</h1>
+                    <h1 className='error'>Something Went Wrong</h1>
                 ) : (
 
                     jobs.map(job => {
